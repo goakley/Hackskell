bdca2f7fa7b579131c418e9c15eead403abc34f5
Glen Oakley <goakley123@gmail.com>
2013-09-16 08:50:21 -0400

    Commented and cleaned up HAssemble

--- src/tools/HAssemble.hs
+++ src/tools/HAssemble.hs
@@ -8,6 +8,7 @@ import System.Environment (getArgs)
 import System.FilePath (addExtension, dropExtension)
 
 
+-- Convert a Word to a padded string of 1s and 0s that represent the number in binary
 word2bin :: Word16 -> String
 word2bin val = if val > 32767 then pady else '0':pady
     where
@@ -18,24 +19,31 @@ word2bin val = if val > 32767 then pady else '0':pady
       pady = replicate (15 - length ib) '0' ++ ib
 
 
-isLeft :: Either a b -> Bool
-isLeft (Left _) = True
-isLeft _ = False
-isRight :: Either a b -> Bool
-isRight (Right _) = True
-isRight _ = False
-
-
 main :: IO ()
 main = do
+  -- Treat all arguments as filenames
   filenamess <- getArgs
-  let filenames = if not (null filenamess) then filenamess else ["_stdin_.asm"]
-  contentss <- if not (null filenamess) then mapM readFile filenames else sequence [getContents]
+  -- If there are no input files, use STDIN as a file
+  let filenames = if not (null filenamess)
+                  then filenamess
+                  else ["_stdin_.asm"]
+  -- Read all the files in; if there are no input files, read STDIN
+  contentss <- if not (null filenamess)
+               then mapM readFile filenames
+               else sequence [getContents]
+  -- Attempt to parse each input file into Assembly
   let parsed = map (second parseAssembly) $ zip filenames contentss
-  let parserrors = filter (\(_,b) -> not (null b)) $ map (\(name,txt) -> either (\a -> (name,a)) (const (name,[])) txt) parsed
-  let asms = filter (\(_,b) -> not (null b)) $ map (\(name,txt) -> either (const (name,[])) (\b -> (name,b)) txt) parsed
+  let parserrors = filter (\(_,b) -> not (null b)) $
+                   map (\(name,txt) -> either (\a -> (name,a)) (const (name,[])) txt) parsed
+  let asms = filter (\(_,b) -> not (null b)) $
+             map (\(name,txt) -> either (const (name,[])) (\b -> (name,b)) txt) parsed
+  -- Attempt to assemble all successfully-parsed files
   let assembled = map (second assemble) asms
-  let asmerrors = filter (\(_,b) -> not (null b)) $ map (\(name,txt) -> either (\a -> (name,a)) (const (name,[])) txt) assembled
-  let asmds = filter (\(_,b) -> not (null b)) $ map (\(name,txt) -> either (const (name,[])) (\b -> (name,b)) txt) assembled
+  let asmerrors = filter (\(_,b) -> not (null b)) $
+                  map (\(name,txt) -> either (\a -> (name,a)) (const (name,[])) txt) assembled
+  let asmds = filter (\(_,b) -> not (null b)) $
+              map (\(name,txt) -> either (const (name,[])) (\b -> (name,b)) txt) assembled
+  -- Display all of the errors encountered when attempting to parse and assemble
   mapM_ (\(a,b) -> (mapM_ (putStrLn . (\c -> a ++ ":" ++ c)) b)) $ parserrors ++ asmerrors
+  -- Output the properly assembled files to their locations
   mapM_ (\(a,b) -> writeFile ((flip addExtension ".hack" . dropExtension) a) (unlines (map word2bin b))) asmds
